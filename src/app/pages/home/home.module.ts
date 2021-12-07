7d5888aaaa3b67ac6a184496c1cf8f8b3fcbd998
Andres Herrera <lherrera>
2021-12-06 20:24:34 -0500

    estructura inicial

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { NavbarModule } from 'src/app/components/shared/navbar/navbar.module';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    NavbarModule
+  ]
+})
+export class HomeModule { }
