c6a41424d8a976512a86ce1b4e168e225e9af965
Andres Herrera <lherrera>
2021-12-10 18:47:05 -0500

    Rutas y servicio auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+    path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)
-  }
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 @NgModule({
