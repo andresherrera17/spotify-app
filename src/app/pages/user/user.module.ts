ac29ec64513ba40da29268664a6b87a536c4a439
Andres Herrera <lherrera>
2021-12-10 19:02:47 -0500

    service auth

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserLoginComponent } from './user-login/user-login.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+
+
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AuthService } from 'src/app/services/auth.service';
 
 @NgModule({
   declarations: [
@@ -11,7 +17,10 @@ import { UserLoginComponent } from './user-login/user-login.component';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
-  ]
+    UserRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule
+  ],
+  providers: [AuthService]
 })
 export class UserModule { }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
+
+  login() {
+    return this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
 }
