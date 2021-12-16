85a89e0cc33874a44e38e456376186e64308c951
Andres Herrera <lherrera>
2021-12-15 19:08:44 -0500

    styles css

--- src/app/pages/home/main/main.component.ts
+++ src/app/pages/home/main/main.component.ts
@@ -8,9 +8,12 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class MainComponent implements OnInit {
 
+  nuevasCanciones: any[] = [];
+  error: boolean;
+  mensajeError: string = "";
+
   constructor(private _serviceSpotify: SpotifyService) {
-
-
+    this.error = false;
   }
 
   ngOnInit(): void {
@@ -18,9 +21,19 @@ export class MainComponent implements OnInit {
   }
 
   getReleases() {
-    this._serviceSpotify.getNewReleases().subscribe((data: any) => {
-      console.log(data)
-    })
+    this._serviceSpotify.getNewReleases().subscribe(
+      {
+        next: (data) => {
+          this.nuevasCanciones = data;
+        },
+        error: ({ error }) => {
+          this.error = true;
+          this.mensajeError = error?.error?.message;
+        }
+      }
+    )
   }
+
+
 
 }
